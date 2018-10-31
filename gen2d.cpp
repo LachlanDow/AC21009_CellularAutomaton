152a24ca7351b3e31e72c8800c1d33d76871cfd1
fpavlica <33609416+fpavlica@users.noreply.github.com>
2018-10-31 18:56:13 +0000

    Game of Life, still needs comments
    to compile use "g++ gen2d.cpp"

---
+++ gen2d.cpp
@@ -0,0 +1,185 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+#include "gen2d.h"
+
+int main(){
+	Gen2d gen2d;
+	gen2d.run();
+	return 1;
+}
+
+void clearScreen()
+{
+	cout << string( 32, '\n' );
+}
+
+Gen2d::Gen2d(){
+	init();
+}
+
+int Gen2d::run() {
+	//tempseed();
+	//generateAndPrint(10);
+	//cin.ignore();
+	randomize();
+	//generateAndPrint(gens);
+	generateAndPrint();
+
+	/*
+	printGrid(parent);
+	tempseed();
+
+	char temp;
+	cin.get(temp);
+	cout << "temp " << temp << (temp == '\n') << endl;
+
+	cin.ignore();
+	printGrid(parent);
+	*/
+	/*
+	cout << "count at (1,2) - " <<count(1,2) <<endl;
+	cout << "count at (0,0) - " <<count(0,0) <<endl;
+	cout << "count at (8,8) - " <<count(8,8) <<endl;
+	*/
+	/*
+	cin.ignore();
+	nextGen();
+	printGrid(child);
+	*/
+	return 1;
+}
+
+
+void Gen2d::init() {
+
+	width = 24;
+	height = 24;
+	gens = 50;
+	parent.resize(height);
+	child.resize(height);
+	//TODO do this in a single for loop
+	for (int i = 0; i<height; i++) {
+		parent.at(i).resize(width);
+		child.at(i).resize(width);
+		for (int j=0; j<width; j++) {
+			parent.at(i).at(j) = 0;
+			child.at(i).at(j) = 0;
+		}
+	}
+}
+
+void Gen2d::randomize(){
+	srand(time(0));
+	for (int i = 0; i<height; i++) {
+		parent.at(i).resize(width);
+		child.at(i).resize(width);
+		for (int j=0; j<width; j++) {
+			parent.at(i).at(j) = (rand()%2);
+			child.at(i).at(j) = 0;
+		}
+	}
+}
+
+void Gen2d::seed(vector<Position> seed){
+	for (auto &pos : seed) {
+		//set valus at pos to 1
+		parent.at(pos.row).at(pos.col) = 1;
+	}
+}
+void Gen2d::gliderseed(){
+	vector<Position> pos(5);
+	pos.at(0).row = 1+3;
+	pos.at(0).col = 2;
+	pos.at(1).row = 2+3;
+	pos.at(1).col = 3;
+	for (int i =2; i < 5; i++) {
+		pos.at(i).row = 3+3;
+		pos.at(i).col = i-1;
+
+	}
+	seed(pos);
+}
+
+
+void Gen2d::generateAndPrint(int numOfGens){
+	for(int i = 0; i < numOfGens; i++) {
+		printGrid(parent);
+		nextGen();
+		parent = child;	
+		std::this_thread::sleep_for(std::chrono::milliseconds(150)); //https://stackoverflow.com/a/10613664
+	}
+}
+
+void Gen2d::generateAndPrint(){
+	char c = '\n';
+	while(c == '\n') {
+		printGrid(parent);
+		nextGen();
+		parent = child;	
+		c = cin.get();
+		//std::this_thread::sleep_for(std::chrono::milliseconds(150)); //https://stackoverflow.com/a/10613664
+	}
+}
+
+void Gen2d::nextGen() {
+
+	for (int row = 0; row<height; row++) {
+		for (int col=0; col<width; col++) {
+			int num = count(row, col); //count always counts parent
+			int newVal;
+			if (num <2 || num >3) { 
+				newVal = 0; //death by underpopulation or overpopulation
+			} else if (num == 3) {
+				newVal = 1; //repopulation
+			} else { //num ==2, the cell stays the same
+				newVal = parent.at(row).at(col);
+			}
+			child.at(row).at(col) = newVal;
+		}
+	}
+}
+
+int Gen2d::count(Position &pos){
+	return count(pos.row, pos.col);
+}
+
+int Gen2d::count(int row, int col){
+	int count = 0;
+
+	//doing (i-1+width)_width to wrap around;
+	count+= parent.at((row-1+width) % width).at((col-1+width) % width); //top left 
+	count+= parent.at((row-1+width) % width).at(col); //top center
+	count+= parent.at((row-1+width) % width).at((col+1) % width); //top right  
+
+	count+= parent.at(row).at((col-1+width) % width); //middle left 
+	count+= parent.at(row).at((col+1) % width); //middle right  
+
+	count+= parent.at((row+1) % width).at((col-1+width) % width); //bottom left 
+	count+= parent.at((row+1) % width).at(col); //bottom center
+	count+= parent.at((row+1) % width).at((col+1) % width); //bottom right  
+	return count;
+}
+
+
+void Gen2d::printGrid(vector<vector<bool>> grid) {
+	clearScreen();
+
+	vector<vector<bool>>::iterator colIt;
+
+	for (colIt=grid.begin(); colIt != grid.end(); ++colIt) {
+		vector<bool>::iterator rowIt;
+		for (rowIt = (*colIt).begin(); rowIt != (*colIt).end(); ++rowIt) {
+			if (*rowIt) {
+				cout << "■ ";
+			} else {
+				cout << "□ ";
+			}
+		}
+		cout << "\n";
+	}
+	cout <<endl;
+}
---
+++ gen2d.h
@@ -0,0 +1,64 @@
+#include <array>
+#include <vector>
+
+using namespace std;
+
+
+
+//int width = 32; //temp, ask for width later
+//int height = 16; //temp, ask for number of generations later
+//const static int ruleSize = 8;
+//int width, height;
+
+/*
+struct ruleBlock {
+	//rename this?
+	array<bool, 3> input;	// 0, 1, 2 correspond to the square to the left, centre, and right
+					// of the child node in the parent
+
+	bool output;	// the result, from the rule
+} ;
+*/
+//ruleBlock rule[ruleSize];
+//bool parent[width], child[width];	//temp ...or not?
+									//probable keep global but allocate memory dynamically
+
+//bool *parent, *child;
+
+
+struct Position{
+	int row;
+	int col;
+};
+
+class Gen2d {
+private:
+	int width, height, gens;
+	//bool *parent, *child;
+	//int seed; //should be an array of size width
+	//ruleBlock rule[ruleSize];
+	vector<vector<bool>> parent, child;
+
+	void init();
+	int count(int row, int col);
+	int count(Position &pos);
+	//void initRule();
+public: 
+	int run();
+	//void printLine(bool line[], int arrayLength);
+	void printGrid(vector<vector<bool>>);
+	void nextGen();
+	void randomize();
+	void generateAndPrint(int);
+	void generateAndPrint();
+	void seed(vector<Position> seed);
+	void gliderseed();
+	//void setRule(int);
+	//void setRule(bool ruleSize[]);
+
+	//void setParams(int,int,int,int);
+
+	Gen2d();
+	//Gen(int, int, int, int); //third int should be an array
+};
+
