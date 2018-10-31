8f2ae5de79480114d4f19d2551adb133c326f8f9
affyzal <32396442+affyzal@users.noreply.github.com>
2018-10-31 03:22:37 +0000

    sub-menus, basic functionality(get and print using inputted data)

--- main.cpp
+++ main.cpp
@@ -3,57 +3,72 @@
 
 using namespace std;
 
-int bsconvertd();
-int dconvertbs();
-int bsconvertv();
+//functions to be added to gen.h
+int getinput1();
+int getinput2();
 int create();
 int load();
+int prmenu(Gen gen);
+
 
 int main() {
 	int menu;
 	do {
-		//int menu;
-		cout << "CELLULAR AUTOMATON\n";
-		cout << "1- Convert Binary String into Decimal\n";
-		cout << "2- Convert Decimal into Binary String\n";
-		cout << "3- Convert Binary Strings into array of variables\n";
-		cout << "4- Create Cellular Automaton\n";
-		cout << "5- Load Saved File\n";
-		cout << "6- Quit\n";
+		cout << "\nCELLULAR AUTOMATON MAIN MENU\n";
+		cout << "1- Create Cellular Automaton\n";
+		cout << "2- Load Saved File\n";
+		cout << "3- Quit\n";
 		cin >> menu;
 
 		switch (menu) {
 		case 1:
-			cout << "Option 1\n";
-			//			bsconvertd();
+			cout << "CREATE CELLULAR AUTOMATON\n";
+			create();	//runs menus and submenus related to creating automaton 1D or 2D
 			break;
 		case 2:
-			cout << "Option 2\n";
-			//			dconvertbs();
+			cout << "LOAD SAVED FILE\n";
+			//load();	Load function not yet implemented
 			break;
 		case 3:
-			cout << "Option 3\n";
-			//			bsconvertv();
-			break;
-		case 4:
-			cout << "Option 4\n";
-						create();
-			break;
-		case 5:
-			cout << "Option 5\n";
-			//			load();
-			break;
-		case 6:
 			break;
 		default:
 			cout << "INVALID CHOICE! RE-Input\n";
 		}
-	} while (menu != 6);
+	} while (menu != 3);
 	return 0;
 }
 
 
+//sub menu for creating automatons
 int create(){
+	int menu;
+	
+	do{
+		cout << "\nCREATING CELLULAR AUTOMATON MENU\n";
+		cout << "1. 1D Cellular Automaton\n";
+		cout << "2. 2D Cellular Automaton\n";
+		cout << "3. Quit\n";
+		cin >> menu;
+
+		switch (menu){
+		case 1:
+			getinput1();	//get inputs for 1D and execute relevant sub menus
+			break;
+		case 2:
+			//getinput2();    get inputs for 2D and execute relevant sub menus
+			break;
+		case 3:
+			break;
+		default:
+			cout << "INVALID INPUT!!! RE-ENTER MENU INPUT: \n";
+		}
+	}while(menu != 3);
+	return 0;
+}
+
+
+//get inputs for 1D automaton
+int getinput1(){
 	int width, height, seed, rule;
 	cout << "Enter your desired width: " << flush;
 	cin >> width;
@@ -71,11 +86,41 @@ int create(){
 	cout << "Ignoring those values and using defaults 32,16,15,39.";
 
 	Gen gen;
-	gen.run();
+//	gen.run();
+	prmenu(gen);		//default vers
 
 	Gen gen2(width, height, seed, rule);
-	gen2.setParams(width, height, seed, rule);
-	gen2.run();
+//	gen2.run();
+	prmenu(gen2);		//user vers
 
+	return 0;
+
+}
+
+int prmenu(Gen gen){
+	int menu;
+
+	cout << "\nPRINT MENU\n";
+	cout << "1. Display Cellular Automaton\n";
+	cout << "2. Display as Decimal Value\n";
+	cout << "3. Quit\n";
+	cin >> menu;
+	//gen.run();
+
+	do{
+		switch(menu){
+			case 1:
+				gen.run();
+				return 0;
+				break;
+			case 2:
+				//DISPLAY AS DECIMAL FUNCTION TO BE RAN
+				break;
+			case 3:
+				break;
+			default:
+				cout << "INVALID INPUT!!! RE-ENTER MENU INPUT: ";
+		}
+	}while(menu != 3);
 	return 0;
 }
