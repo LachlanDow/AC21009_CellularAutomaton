7a45bcf2ec62bc6fcd9908fe916e345b1a856fdd
affyzal <32396442+affyzal@users.noreply.github.com>
2018-11-01 21:37:30 +0000

    added internal commentary

--- main.cpp
+++ main.cpp
@@ -14,42 +14,42 @@ int gol();
 
 int main() {
 	int menu;
-	do {
-		cout << "\nCELLULAR AUTOMATON MAIN MENU\n" << flush;
+	do {				//do while for menu
+		cout << "\nCELLULAR AUTOMATON MAIN MENU\n" << flush;	//display menu options
 		cout << "1- Create Cellular Automaton\n" << flush;
 		cout << "2- Game of Life\n" << flush;
 		cout << "3- Quit\n" << flush;
-		cin >> menu;
-		cout << "\033[2J\033[1;1H" << flush;
+		cin >> menu;			//get input
+		cout << "\033[2J\033[1;1H" << flush;	//clear screen
 
-		switch (menu) {
-		case 1:
+		switch (menu) {		//switch menu
+		case 1:		//create cellular automaton
 			cout << "CREATE CELLULAR AUTOMATON\n" << flush;
 			ca();	//runs menus and submenus related to creating automaton 1D or 2D
 			break;
-		case 2:
+		case 2:			//game of life
 			cout << "GAME OF LIFE\n" << flush;
 			gol();	//create 2D
 			break;
-		case 3:
+		case 3:			//quit
 			break;
 		default:
 			cout << "INVALID CHOICE! RE-Input\n" << flush;
 		}
-	} while (menu != 3);
+	} while (menu != 3);		//termination
 	return 0;
 }
 
 //1D automaton
 int ca(){
-	cout << "\033[2J\033[1;1H" << flush;
-	int width, height, rule;
+	cout << "\033[2J\033[1;1H" << flush;		//clear
+	int width, height, rule;			//initialise variables
 
 
 	
 	
-	bool valid = false;
-	do{
+	bool valid = false;	//validatation variables
+	do{			//runs until correct input
 		cout << "Enter your desired width: " << flush;
         cin >> width;
         if (cin.good())
@@ -69,7 +69,7 @@ int ca(){
 
 
 
-	valid = false;
+	valid = false;			//variable for validation
 	do{
 	cout << "Enter your desired height(number of generations): " << flush;
         cin >> height;
@@ -89,15 +89,16 @@ int ca(){
     } while (!valid);
 
 
-	cout << "Where do you want your seeds (positions separated by spaces)? " << flush;
+	cout << "Where do you want your seeds (positions separated by spaces)? " << flush; //get input for seeds as line
 	string line;
 	cin.ignore();
 	getline(cin, line);
 	int number;
+	
 	vector<int> seed;
 	stringstream iss(line);
 
-	while (iss) {
+	while (iss) {			//separate at spaces for seed options and validate for data type
 		if (iss >> number) { //https://stackoverflow.com/a/20659156
 			seed.push_back( number );
 		} else {
@@ -112,8 +113,8 @@ int ca(){
 	}
 >>>>>>> dd5723993041240e9f14edccb4047707d761ca81
 
-    valid = false;
-	do{
+    valid = false;			//variable for validation
+	do{			//runs until valid
 	cout << "What rule do you want to use? " << flush;
         cin >> rule;
         if (cin.good())
@@ -131,8 +132,8 @@ int ca(){
         }
     } while (!valid);
 
-	Gen gen2(width, height, seed, rule);
-	gen2.run();		//user vers
+	Gen gen2(width, height, seed, rule);	//generate automaton
+	gen2.run();		//display automaton
 
 	return 0;
 
@@ -141,16 +142,16 @@ int ca(){
 
 //Game of life
 int gol(){
-	cout << "\033[2J\033[1;1H" << flush;
+	cout << "\033[2J\033[1;1H" << flush;	//clear screen
 	int width, height;
-	cout << "Enter your desired width: " << flush;
+	cout << "Enter your desired width: " << flush;	//get width input
 	cin >> width;
 
-	cout << "Enter your desired height: " << flush;
+	cout << "Enter your desired height: " << flush; //get height input
 	cin >> height;
 
-	Gen2d gen2d(width,height);
-	gen2d.run();
+	Gen2d gen2d(width,height);		//generate game of life
+	gen2d.run();			//run game of life
 	return 1;
 }
 
